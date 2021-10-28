6e10f86053a219f1065a1f0d4654f7b7d8c5e24d
larycoder <lenhuchuhiep99@gmail.com>
2021-10-28 16:20:45 +0700

    LH    : add new resource for diary and task access

--- backend/resources/note/DiaryResource.py
+++ backend/resources/note/DiaryResource.py
@@ -1,35 +1,35 @@
 from flask_restx import Resource, reqparse
 from flask_restx import inputs
 
-from repositories.TaskRepo import TaskRepo
-from models.TaskModel import TaskModel
+from repositories.DiaryRepo import DiaryRepo
+from models.DiaryModel import DiaryModel
 from models.ResponseModel import ResponseModel
 
 from utils.JsonEncoder import JsonEncoder
+from utils.TimeUtil import TimeUtil
 
 
-class TaskResource(Resource):
+class DiaryResource(Resource):
     def get(self):
-        repo = TaskRepo()
+        repo = DiaryRepo()
         data_list = repo.list_all()
         return JsonEncoder.encode(ResponseModel(data_list))
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('task', required=True, type=str)
+        parser.add_argument('activity', required=True, type=str)
         parser.add_argument('notes', type=str)
-        parser.add_argument('label', type=str)
-        parser.add_argument('priority', type=str)
-        parser.add_argument('deadline', type=inputs.datetime_from_iso8601)
-        parser.add_argument('measurement', type=str)
-        parser.add_argument('process', type=float)
-
+        parser.add_argument('start_time', type=inputs.datetime_from_iso8601)
+        parser.add_argument('duration', type=str)
+        parser.add_argument('taskId', type=int)
         args = parser.parse_args()
 
-        model = TaskModel()
+        args['duration'] = TimeUtil.str_to_second(args['duration'])
+
+        model = DiaryModel()
         model.loadJSON(args)
 
-        repo = TaskRepo()
+        repo = DiaryRepo()
         repo.add_object(model)
         repo.commit()
         return None
--- backend/resources/note/__init__.py
+++ backend/resources/note/__init__.py
@@ -2,9 +2,13 @@ from flask import Blueprint
 from flask_restx import Api
 
 from resources.note.NoteResource import NoteResource
+from resources.note.TaskResource import TaskResource
+from resources.note.DiaryResource import DiaryResource
 
 note_bp = Blueprint('noteApi', __name__,
                     static_folder='static', static_url_path='home')
 api = Api(note_bp)
 
-api.add_resource(NoteResource, '/notes')
+api.add_resource(NoteResource, '/note')
+api.add_resource(TaskResource, '/task')
+api.add_resource(DiaryResource, '/diary')
