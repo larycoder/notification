c283d9a7f01b15d54592ff4d4d1c733a527e0f49
larycoder <lenhuchuhiep99@gmail.com>
2021-12-01 12:38:25 +0700

    LH    : move sensitive configure to ignore file

--- backend/db/engine.py
+++ backend/db/engine.py
@@ -1,6 +1,17 @@
 from sqlalchemy import create_engine
+import json
+import sys
 
+# load config
+config = {}
+with open("./config.json", "r") as jsonFile:
+    text = jsonFile.read()
+    config = json.loads(text)
+
+if not config:
+    print("DB could not load configuration...")
+    sys.exit(1)
 
 # sqlite engine
-sql_connection = "mysql://username:password@127.0.0.1:3306/notification"
+sql_connection = config["db"]["connection"]
 engine = create_engine(sql_connection, echo=True, future=True)
