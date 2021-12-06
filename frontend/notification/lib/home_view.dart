11948bbf5f9a0ea268cf0ac26567d9101330455f
larycoder <lenhuchuhiep99@gmail.com>
2021-12-06 22:50:58 +0700

    LH    : add home view

---
+++ frontend/notification/lib/home_view.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  Widget _drawerOption(String text) {
+    return Text(text, style: const TextStyle(fontSize: 16.0));
+  }
+
+  Widget _mainButtonFragment(BuildContext context) {
+    return SizedBox(
+      height: 70.0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ElevatedButton(
+            child: const Text('Diary'),
+            onPressed: () {},
+          ),
+          ElevatedButton(
+            child: const Text('Task'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Home")),
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                child: Icon(Icons.person),
+              ),
+              accountName: Text("User Name"),
+              accountEmail: Text("notification.app@email.com"),
+            ),
+            ListTile(
+              leading: const Icon(Icons.check),
+              title: _drawerOption("Authentication"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: _drawerOption("Setting"),
+              onTap: () {},
+            ),
+          ],
+        )),
+        body: Stack(
+          children: [
+            Center(child: _mainButtonFragment(context)),
+          ],
+        ));
+  }
+}
