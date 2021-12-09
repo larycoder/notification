537aec37f0a966c869dfd9e823211e527b655b0e
larycoder <lenhuchuhiep99@gmail.com>
2021-12-09 11:35:45 +0700

    LH    : add dashboard list note

--- frontend/notification/lib/pages/note/dashboard_view.dart
+++ frontend/notification/lib/pages/note/dashboard_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notification/models/task_model.dart';
+
+import 'note_view.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({Key? key}) : super(key: key);
@@ -8,22 +11,32 @@ class DashboardView extends StatefulWidget {
 }
 
 class _DashboardViewState extends State<DashboardView> {
-  String noteType = "diary";
+  String? noteType;
 
   Widget _noteListFragment(BuildContext context) {
-    final items =
-        List<String>.generate(100, (i) => "item $i"); // for debug only
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return ListTile(title: Text(items[index]));
-      },
+    // replace with repository class
+    final TaskModel data = TaskModel(task: "Flutter", notes: "Test");
+    final items = List<TaskNote>.generate(
+      100,
+      (i) => TaskNote(data: data),
+    );
+    return Card(
+      color: Colors.grey[300],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(title: items[index]);
+        },
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    noteType = ModalRoute.of(context)!.settings.arguments as String;
+    noteType = ModalRoute.of(context)!.settings.arguments as String? ?? "diary";
     return Scaffold(
       appBar: AppBar(title: Text("Dashboard $noteType")),
       body: _noteListFragment(context),
