3248df13313df51d5b222ed8b74ffc1f8e53b5c3
larycoder <lenhuchuhiep99@gmail.com>
2022-03-22 13:10:11 +0700

    LH    : add note widget for task detail

--- frontend/notification/lib/task/task_view.dart
+++ frontend/notification/lib/task/task_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:notification/constant/type.dart';
 import 'package:notification/models/task_model.dart';
+
+import 'note_fragment.dart';
 
 /// Detail view of task
 class TaskView extends StatelessWidget {
@@ -11,9 +15,18 @@ class TaskView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(data.task!)),
-      body: Text("localId: $localId"),
-    );
+        appBar: AppBar(title: Text(data.task!)),
+        body: Column(
+          children: [
+            NoteFragment(data),
+            TextButton(
+                child: const Text("Save"),
+                onPressed: () {
+                  Hive.box(DataType.task).put(localId, data);
+                  Navigator.pop(context);
+                }),
+          ],
+        ));
   }
 
   /// Clone new task view from current one
