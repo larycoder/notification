e4c1a1f903bb7c3761b6a620bd9918d8853fbebb
larycoder <lenhuchuhiep99@gmail.com>
2023-09-28 03:35:30 +0200

    src: simple ORM code for sqlite connection and data model

--- src/main.py
+++ src/main.py
@@ -1,39 +1,10 @@
-from flask import Flask, send_file
-from flask import Blueprint
-from resources.note import note_bp
+from application import app
 
+# sql engine
+import sql_db as sql
 
-def init_app():
-    app = Flask(__name__)
-
-    # Simple hello view to init app
-    @app.route('/hello')
-    def hello():
-        return 'hello'
-
-    @app.route('/favicon.ico')
-    def favicon():
-        return send_file('favicon.ico')
-
-    # Blueprint for statics folder
-    static_bp = Blueprint(
-        'statics', __name__,
-        static_folder='statics',
-        static_url_path='/statics'
-    )
-    app.register_blueprint(static_bp)
-
-    # Resource
-    app.register_blueprint(note_bp, url_prefix="/noteApi")
-
-    return app
-
-
-if __name__ == '__main__':
-    app = init_app()
-
+if __name__ == "__main__":
     debug = True
-    host = '0.0.0.0'
+    host = "0.0.0.0"
     port = 8080
-
     app.run(debug=debug, host=host, port=port)
---
+++ src/sql_db.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from application import app
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notification.db"
+db = SQLAlchemy(app)
+
+note_tag = db.Table(
+    "note_tag",
+    db.Column("note_id", db.Integer, db.ForeignKey("note.id")),
+    db.Column("tag_id", db.Integer, db.ForeignKey("tag.id")),
+)
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(500), nullable=False)
+    content = db.Column(db.String(5000), nullable=True)
+    tags = db.relationship("Tag", secondary=note_tag, backref="notes")
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+
+def generate_db():
+    db.drop_all()
+    db.create_all()
+
+
+if __name__ == "__main__":
+    print("[Test 1] Generate database...")
+    generate_db()
