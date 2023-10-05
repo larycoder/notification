cc805b030fa0069afea8f9b9248a085e90227a98
larycoder <lenhuchuhiep99@gmail.com>
2023-10-06 03:23:40 +0700

    src: implement CRUD for single note

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -27,9 +27,70 @@ async function fetch_note_add(item) {
   return resp.json();
 }
 
+async function fetch_note_get(note_id) {
+  let resp = await fetch(`/api/note/${note_id}`, {
+    "method": "GET"
+  });
+  return resp.json();
+}
+
+async function fetch_note_del(note_id) {
+  let resp = await fetch(`/api/note/${note_id}`, {
+    "method": "DELETE"
+  });
+  return resp.json();
+}
+
+async function fetch_note_patch(note_id, item) {
+  let resp = await fetch(`/api/note/${note_id}`, {
+    "method": "PATCH",
+    "headers": {
+      "content-type": "application/json"
+    },
+    "body": JSON.stringify(item)
+  });
+  return resp.json();
+}
+
 /*
  * DOM modification functions
  * */
+
+async function dom_note_view_make(note_id) {
+  let note = await fetch_note_get(note_id);
+  $("#note-subject-readonly")[0].value = note.subject;
+  tinyMCE.get("note-editor-readonly").setContent(note.content);
+  $("#note-rw")[0].style.display = "none";
+  $("#note-readonly")[0].style.display = "block";
+  $("#note-header")[0].style.display = "block";
+}
+
+async function dom_note_edit_make(note_id) {
+  let note = await fetch_note_get(note_id);
+  $("#note-subject")[0].value = note.subject;
+  tinyMCE.get("note-editor").setContent(note.content);
+  $("#note-rw-save")[0].setAttribute("data-note-id", note_id);
+  $("#note-readonly")[0].style.display = "none";
+  $("#note-header")[0].style.display = "block";
+  $("#note-rw")[0].style.display = "block";
+}
+
+async function dom_note_del(note_id) {
+  let table = $("#note-table")[0];
+  let row_idx = $(`#notes-table-row-${note_id}`)[0].rowIndex;
+  await fetch_note_del(note_id);
+  table.deleteRow(row_idx);
+}
+
+async function dom_notes_table_opts(note_id) {
+  let opts = document.createElement("div");
+  let opt_attrs = `class="btn btn-primary my-2"`;
+  let view = `<button ${opt_attrs} onclick="dom_note_view_make(${note_id})">view</button>`;
+  let edit = `<button ${opt_attrs} onclick="dom_note_edit_make(${note_id})">edit</button>`;
+  let del = `<button ${opt_attrs} onclick="dom_note_del(${note_id})">del</button>`;
+  opts.innerHTML = `${view} ${edit} ${del}`;
+  return opts;
+}
 
 async function dom_notes_table_make() {
   var fields = ["id", "subject", "action"];
@@ -51,12 +112,15 @@ async function dom_notes_table_make() {
   let notes = await fetch_notes_list();
   for (let note of notes) {
     let row = document.createElement("tr");
+    row.setAttribute("id", `notes-table-row-${note.id}`)
     for (let field of fields) {
       if (field == "id") {
         let row_hdr = document.createElement("th");
         row_hdr.setAttribute("scope", "row");
         row_hdr.innerText = note.id;
         row.appendChild(row_hdr);
+      } else if (field == "action") {
+        row.appendChild(await dom_notes_table_opts(note.id));
       } else {
         let row_dat = document.createElement("td");
         row_dat.innerText = note[field];
@@ -68,13 +132,10 @@ async function dom_notes_table_make() {
   table.appendChild(body);
 }
 
-async function dom_note_add() {
-  console.log("add new note");
-}
-
 async function dom_actions_add_onclick() {
   $("#action-notes-refresh").click(() => {
-
+    $("#note-table")[0].innerHTML = '';
+    dom_notes_table_make();
   });
   $("#action-notes-delete").click(async () => {
     await fetch_notes_del();
@@ -85,7 +146,6 @@ async function dom_actions_add_onclick() {
     $("#note-readonly")[0].style.display = "none";
     if ($("#note-header")[0].style.display == "none") {
       $("#note-rw-save")[0].setAttribute("data-note-id", "-1");
-      $("#note-rw-save").click(dom_note_add);
       $("#note-header")[0].style.display = "block";
       $("#note-rw")[0].style.display = "block";
     } else {
@@ -98,5 +158,18 @@ async function dom_actions_add_onclick() {
     tinyMCE.get("note-editor").setContent("");
     $("#note-header")[0].style.display = "none";
     $("#note-rw")[0].style.display = "none";
+  });
+  $("#note-rw-save").click(async () => {
+    let id = $("#note-rw-save")[0].getAttribute("data-note-id");
+    let item = {
+      "subject": $("#note-subject")[0].value,
+      "content": tinyMCE.get("note-editor").getContent(),
+    };
+    if (id == -1)
+      await fetch_note_add(item);
+    else
+      await fetch_note_patch(id, item);
+    $("#note-rw-discard").click();
+    $("#action-notes-refresh").click();
   });
 }
