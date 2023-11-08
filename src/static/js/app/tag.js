8df46d07611ffab3793b6889dd3d57091d578786
larycoder <lenhuchuhiep99@gmail.com>
2023-11-08 14:33:14 +0700

    src: show list of tag in frontend

--- src/static/js/app/tag.js
+++ src/static/js/app/tag.js
@@ -12,5 +12,16 @@ async function fetch_tags_list() {
  * DOM modification functions
  * */
 async function dom_list_tags_make() {
-  /* TODO: build tag list */
+  let tags = await fetch_tags_list();
+  for (let tag of tags) {
+    let dom_tag_str = `
+    <li class="nav-item">
+      <p><span class="fa fa-tag"></span> ${tag.name}</p>
+    </li>
+    `;
+    let dom_tag = new DOMParser()
+      .parseFromString(dom_tag_str, "text/html")
+      .body.firstElementChild;
+    $("#tags-list")[0].appendChild(dom_tag);
+  }
 }
